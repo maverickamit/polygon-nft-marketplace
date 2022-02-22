6eba92d93c4daa0acca779fb3e09780825212c58
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-02-22 10:30:17 +0530

    feat: add a navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import Link from "next/link";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <div className="flex mt-4">
+          <p className="text mr-6 font-bold">NFT Marketplace</p>
+          <Link href="/">
+            <a className="mr-6 text-blue-500">Home</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-blue-500">Sell Digital Asset</a>
+          </Link>{" "}
+          <Link href="/my-assets">
+            <a className="mr-6 text-blue-500">My Digital Assets</a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-blue-500">Creator Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
