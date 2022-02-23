0c04d19c49a1d67011bb8e9bf10b21e6e995692d
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-02-24 00:56:42 +0530

    feat: fetch and display current marketplace items

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,77 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+import { nftAddress, nftMarketplaceAddress } from "../config";
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import NFTMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
+import { ethers } from "ethers";
 export default function Home() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  const loadNfts = async () => {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider);
+    const marketPlaceContract = new ethers.Contract(
+      nftMarketplaceAddress,
+      NFTMarketplace.abi,
+      provider
+    );
+    const data = await marketPlaceContract.fetchMarketItems();
+    console.log("length" + data.length);
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenID);
+        console.log(tokenUri);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.url,
+          name: meta.data.title,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState("loaded");
+  };
+  useEffect(() => {
+    loadNfts();
+  }, []);
+  if (loadingState === "loaded" && !nfts.length) {
+    return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>;
+  }
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>NFT Market Place</title>
       </Head>
-      <h1>Welcome to NFT Market Place!</h1>
+      <div className="flex justify-center">
+        <div className="px-4" style={{ maxWidth: "1600px" }}>
+          <div className="grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            {nfts.map((nft, i) => {
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <Image src={"https://via.placeholder.com/600/92c952"} alt="" />
+                <div className="" p-4>
+                  <p
+                    styles={{ heigt: "64px" }}
+                    className="text-2xl font-semibold"
+                  >
+                    {"nft.name"}
+                  </p>
+                </div>
+              </div>;
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
